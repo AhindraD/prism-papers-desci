ca1b04f4cbae6fef0bd16acc73c9db213e5af3e8
Ahindra D <official.ahindra@gmail.com>
2025-03-05 04:37:34 +0530

    modifying tests

--- anchor/tests/prismpapersdesci.spec.ts
+++ anchor/tests/prismpapersdesci.spec.ts
@@ -1,17 +1,112 @@
 import * as anchor from '@coral-xyz/anchor'
-import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
-import {Prismpapersdesci} from '../target/types/prismpapersdesci'
+import { Program, BN } from '@coral-xyz/anchor'
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  SystemProgram,
+} from '@solana/web3.js'
+import {
+  confirmTransaction,
+  getLogs,
+  makeKeypairs,
+  getExplorerLink
+} from '@solana-developers/helpers';
+import { Prismpapersdesci } from '../target/types/prismpapersdesci';
+import assert from 'assert';
+// import { before, beforeEach, it } from 'mocha';
+// import { assert, expect } from 'chai';
 
 describe('prismpapersdesci', () => {
   // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
+  const cluster_for_explorerLink = 'localnet';
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const connection = provider.connection;
+  const programId = new PublicKey('5MRvAZkDQK1u27EEzrdpKS6uUKsGqEPRkztZVapcTcLv');
+  const program = anchor.workspace.Prismpapersdesci as Program<Prismpapersdesci>;
+  // const payer = provider.wallet as anchor.Wallet;
+  // const prismpapersdesciKeypair = Keypair.generate();
 
-  const program = anchor.workspace.Prismpapersdesci as Program<Prismpapersdesci>
+  //Create Admin keypair
+  const admin = Keypair.generate();
+  //Create User (Publisher) keypair
+  const walter = Keypair.generate();
+  //Create User (Reviewer #1) keypair;
+  const jimmy = Keypair.generate();
+  //Create User (Reviewer #2) keypair;
+  const jesse = Keypair.generate();
+  //Create User (Buyer) keypair
+  const mike = Keypair.generate();
 
-  const prismpapersdesciKeypair = Keypair.generate()
+  beforeAll(async () => {
+    console.log(`airdroping...`);
+    const all_wallets = [admin, walter, jimmy, jesse, mike];
+    for (const wallet of all_wallets) {
+      const tx = await provider.connection.requestAirdrop(
+        wallet.publicKey,
+        5 * LAMPORTS_PER_SOL
+      );
+      await confirmTransaction(connection, tx, 'confirmed');
+      // console.log(`Airdropped ${5} SOL to ${wallet.publicKey}.`);
+    }
+    console.log(`airdrop complete ---> funded all wallets.`);
+  });
 
 
+  it(`Admin Initialized PrismPapers Platform Successfully.`, async () => {
+    // console.log(`<----------- Initializing PrismPapers Platform ------------>`);
+    try {
+      const initilialize = await program.methods
+        .initialize()
+        .accountsPartial({
+          admin: admin.publicKey,
+        })
+        .instruction();
+
+      const blockhashContext = await connection.getLatestBlockhash();
+
+      const txn = new anchor.web3.Transaction({
+        feePayer: admin.publicKey,
+        blockhash: blockhashContext.blockhash,
+        lastValidBlockHeight: blockhashContext.lastValidBlockHeight,
+      }).add(initilialize);
+
+      const signature = await anchor.web3.sendAndConfirmTransaction(
+        connection,
+        txn,
+        [admin]
+      );
+      // console.log(`Platform initialized!:  ${getExplorerLink("tx", signature, cluster_for_explorerLink)}`);
+    } catch (err) {
+      console.log(`Failed to initialize Platform: ${err}`);
+    }
+
+    //Check Platform Config PDA address
+    const [configAccountAdress, bump] = await PublicKey.findProgramAddressSync(
+      [Buffer.from("platform_config"), admin.publicKey.toBuffer()],
+      programId
+    );
+    // console.log(
+    //   `
+    //     Platform Config PDA: ${configAccountAdress.toString()}
+    //     Bump: ${bump}
+    //   `
+    // );
+    // Fetch the config PDA
+    const configAccount = await program.account.platformConfig.fetch(
+      configAccountAdress
+    );
+
+    // Convert admin.publicKey and configAccount.admins to strings for comparison
+    const adminPubKey = admin.publicKey.toString();
+    const configAdminPubKey = configAccount.admin.toString();
+
+    // Check if the admin's public key is in the config admins list
+    assert.equal(
+      adminPubKey,
+      configAdminPubKey,
+      "Admin public key not found in PrismPapers Platform admins list!"
+    );
+  });
 })
